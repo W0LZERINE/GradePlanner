1cd626bfab66881066f457c6c35e6fcc64c177a4
Sr Sanz <itzansanzfernandez@gmail.com>
2026-09-04 19:26:14 +0200

    commit extra

--- src/expediente.py
+++ src/expediente.py
@@ -5,4 +5,7 @@ class Expediente:
     def __init__(self,name):
         self._name = name
         self._asignaturas = [] #Array de assignaturas
-        
+
+
+    def set_name(self,name):
+        self._name = name
--- src/main.py
+++ src/main.py
@@ -1,6 +1,13 @@
 ### MAIN ###
 
 from expediente import *
+from menus import *
 
+while True:
+    menu_base()
 
+    opcion = input_with_control()
 
+    match opcion :
+        case 0:
+            pass
