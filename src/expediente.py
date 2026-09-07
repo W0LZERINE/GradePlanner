ce926c35fd52198ac1698cd13106a01f5802abbc
Sr Sanz <itzansanzfernandez@gmail.com>
2026-09-07 19:30:36 +0200

    Creados los métodos add y remove asignatura y retoques en metodo remove_evaluation

--- src/expediente.py
+++ src/expediente.py
@@ -10,3 +10,14 @@ class Expediente:
 
     def set_name(self,name):
         self._name = name
+
+    def añadir_asignatura(self,asignatura):
+        self._asignaturas.append(asignatura)
+
+    def eliminar_asignatura(self,input):
+        try:
+            self._asignaturas.remove(input)
+        except ValueError:
+            print("Error: no se ha podido eliminar esa asignatura, no existe.\n")
+
+    
